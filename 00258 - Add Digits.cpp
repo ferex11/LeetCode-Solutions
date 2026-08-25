23825461600e99c05766152dd79df589eaa7723f
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-25 13:37:53 +0530

    Update and rename 258-Add Digits.cpp to 00258 - Add Digits.cpp

--- 00258 - Add Digits.cpp
+++ 00258 - Add Digits.cpp
@@ -3,16 +3,13 @@ public:
     int addDigits(int num) {
         int sum = 0;
         while (num){
-
             sum += (num % 10);
             num /= 10;
-
         }
         if (sum < 10)
             return sum ;        
         else
             return addDigits(sum);
-        
     }
     
 };
